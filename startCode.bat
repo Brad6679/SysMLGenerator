5b6dea76d62d46499690d038f3256e0559aaa19f
Brady Jugan <34609432+Brad6679@users.noreply.github.com>
2024-07-16 21:45:42 -0400

    Update startCode.bat

--- startCode.bat
+++ startCode.bat
@@ -1,14 +1,10 @@
-:: %windir%\System32\cmd.exe "/K" C:\Users\brady\anaconda3\Scripts\activate.bat C:\Users\brady\anaconda3
-:: activate NSIEnv
-:: python sysmlGPTUI.py
-
 @echo off
 
 :: Set the path to the conda executable
 set CONDA_PATH=C:\Users\<User>\anaconda3\Scripts\activate.bat
 
 :: Activate the NSIenv environment
-call "%CONDA_PATH%" activate NSIenv
+call "%CONDA_PATH%" activate <Env>
 
 :: Run the python script
 python "sysmlGPTUI.py"
