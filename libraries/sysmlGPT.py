80d622d40a7bfce81ebd4b8ddb854ca40148d54b
Brady Jugan <34609432+Brad6679@users.noreply.github.com>
2024-07-26 17:36:25 -0400

    Update sysmlGPT.py
    Added docstring

--- libraries/sysmlGPT.py
+++ libraries/sysmlGPT.py
@@ -1,16 +1,23 @@
-import base64
-import requests
-import glob
-import os
 from openai import OpenAI
 import time
-import re
+import re 
 
-
-
+"""
+OpenAI Handling Class
+"""
 class sysmlGPT():
+
+  """ 
+      Initializes sysmlGPT object
+
+      asst : str
+            Assistant to use (image or text based)
+  """
   def __init__(self, asst):
+      
       if asst == "Default":
+        self.asstid = "asst_JaH0siFJiK3oXZUKD1G3WZCy"
+      elif asst == "Image":
         self.asstid = "asst_rkxmYDfYwpz7DH6x5MIFL2ID"
       else:
         self.asstid = None # Smart city assistant unimplemented
@@ -21,23 +28,26 @@ class sysmlGPT():
 
       self.thread = self.client.beta.threads.create()
 
-  
+  """ 
+      Sends a query to ChatGPT using API
 
-  # Function to encode the image
-  def encode_image(self, image_path):
-    with open(image_path, "rb") as image_file:
-      return base64.b64encode(image_file.read()).decode('utf-8')
+      prompt : str
+            Textual prompt to input to GPT, None if inputting img
+
+      img : str
+            Image path to input to GPT, None if inputting text
 
 
+      Returns : str
+        Message response from ChatGPT
+  """
   def run(self, prompt, img):
     if prompt == None and img == None:
-      #sys.exit("System exited. Input image or text")
-      return "System exited. Input image or text"
+      raise RuntimeError("ERROR: No text or image inputted but request submitted. Exiting.")
 
     if img != None:
       if prompt == None:
         prompt = "Using SysMLv2, generate code that describes the system depicted in this image."
-      #base64_image = encode_image(image_path)
       file = self.client.files.create(
       file=open(img, "rb"),
       purpose="vision"
@@ -90,14 +100,17 @@ class sysmlGPT():
         message_id=messages.first_id
       )
   
-    #d = content#messages.content[0].text
-    
-    #for message in messages.data:
-     # d.append(message.content[0])
     return message.content[0].text.value
       
 
+  """ 
+      Extracts code from GPT response using regular expression
+
+      text : str
+            GPT response text
+  """
   def extract_code_blocks(self, text):
+    
     # Regular expression to match code blocks
     code_block_pattern = re.compile(r'```(\w+)\n(.*?)```', re.DOTALL)
     
@@ -109,12 +122,7 @@ class sysmlGPT():
         # match[0] is the language (e.g., 'py')
         # match[1] is the code
         code_blocks = match[1]
-        #print(match[1])
     
     return code_blocks
 
-#sys = sysmlGPT('Default')
-#out = sys.run("Generate sysmlv2 code that models a car", None)
-#out = sys.run("Generate sysmlv2 that models the system described in this image", "C:\\Users\\brady\\Downloads\\92bc146b.jpg")
-#print(out)
-#print(out.text)#message.content[0].text
+
